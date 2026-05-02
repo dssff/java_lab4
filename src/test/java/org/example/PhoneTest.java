eae90572e44978db5605746efd3d437262085e26
dssff <dendssff@gmail.com>
2026-05-02 22:31:12 +0300

    Update tests

--- src/test/java/org/example/PhoneTest.java
+++ src/test/java/org/example/PhoneTest.java
@@ -10,38 +10,57 @@ class PhoneTest {
 
     @Test
     void shouldCreatePhoneWithValidData() {
-        Phone phone = new Phone("Apple", "iPhone 17", 799.99, 2025, 256, 3200, "iOS", 170.5, "Black");
+        Processor processor = new Processor("A19", 8, 3.8);
+        Phone phone = new Phone("Apple", "iPhone 17", 799.99, 2025, 256, 3200, OperatingSystem.IOS, 170.5, Color.BLACK, processor);
+        
         assertEquals("Apple", phone.getBrand());
         assertEquals(799.99, phone.getPrice());
+        assertEquals(OperatingSystem.IOS, phone.getOperatingSystem());
+        assertEquals(Color.BLACK, phone.getColor());
+        assertEquals(processor, phone.getProcessor());
     }
 
     @Test
     void shouldThrowExceptionWhenEmptyBrandInConstructor() {
+        Processor processor = new Processor("A19", 8, 3.8);
         assertThrows(IllegalArgumentException.class, () -> {
-            new Phone("", "iPhone 17", 799.99, 2025, 256, 3200, "iOS", 170.5, "Black");
+            new Phone("", "iPhone 17", 799.99, 2025, 256, 3200, OperatingSystem.IOS, 170.5, Color.BLACK, processor);
         });
     }
 
     @Test
     void shouldThrowExceptionWhenNegativePriceInConstructor() {
+        Processor processor = new Processor("A19", 8, 3.8);
         assertThrows(IllegalArgumentException.class, () -> {
-            new Phone("Apple", "iPhone 17", -1.0, 2025, 256, 3200, "iOS", 170.5, "Black");
+            new Phone("Apple", "iPhone 17", -1.0, 2025, 256, 3200, OperatingSystem.IOS, 170.5, Color.BLACK, processor);
         });
     }
 
     @Test
-    void shouldThrowExceptionWhenInvalidValueInSetter() {
-        Phone phone = new Phone("Apple", "iPhone 17", 799.99, 2025, 256, 3200, "iOS", 170.5, "Black");
-        assertThrows(IllegalArgumentException.class, () -> {
-            phone.setPrice(-100.0);
-        });
+    void shouldCorrectlyCopyPhoneUsingCopyConstructor() {
+        Processor processor = new Processor("A19", 8, 3.8);
+        Phone original = new Phone("Apple", "iPhone 17", 799.99, 2025, 256, 3200, OperatingSystem.IOS, 170.5, Color.BLACK, processor);
+        Phone copy = new Phone(original);
+
+        assertEquals(original, copy);
+        assertNotSame(original, copy);
+        assertNotSame(original.getProcessor(), copy.getProcessor()); // Deep copy of processor
+        assertEquals(original.getProcessor().getModel(), copy.getProcessor().getModel());
     }
 
     @Test
-    void shouldThrowExceptionWhenEmptyBrandInSetter() {
-        Phone phone = new Phone("Apple", "iPhone 17", 799.99, 2025, 256, 3200, "iOS", 170.5, "Black");
+    void shouldIncrementStaticCounter() {
+        int initialCount = Phone.getCount();
+        Processor processor = new Processor("A19", 8, 3.8);
+        new Phone("Apple", "iPhone 17", 799.99, 2025, 256, 3200, OperatingSystem.IOS, 170.5, Color.BLACK, processor);
+        
+        assertEquals(initialCount + 1, Phone.getCount());
+    }
+
+    @Test
+    void shouldThrowExceptionWhenProcessorIsNull() {
         assertThrows(IllegalArgumentException.class, () -> {
-            phone.setBrand("");
+            new Phone("Apple", "iPhone 17", 799.99, 2025, 256, 3200, OperatingSystem.IOS, 170.5, Color.BLACK, null);
         });
     }
 }
