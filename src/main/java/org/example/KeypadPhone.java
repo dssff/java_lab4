66283902062951b62cbc833489415247b7922033
dssff <dendssff@gmail.com>
2026-05-03 19:38:59 +0300

    Add derived classes for base model

---
+++ src/main/java/org/example/KeypadPhone.java
@@ -0,0 +1,44 @@
+package org.example;
+
+/**
+ * Клас для кнопкочних телефонів (похідний від Phone).
+ */
+public class KeypadPhone extends Phone {
+    private boolean hasDualSim;
+    private boolean hasFlashlight;
+
+    public KeypadPhone(String brand, String model, double price, int year, int storage,
+            int batteryCapacity, OperatingSystem operatingSystem, double weight, Color color,
+            boolean hasDualSim, boolean hasFlashlight) {
+        super(brand, model, price, year, storage, batteryCapacity, operatingSystem, weight, color);
+        this.hasDualSim = hasDualSim;
+        this.hasFlashlight = hasFlashlight;
+    }
+
+    public boolean isHasDualSim() {
+        return hasDualSim;
+    }
+
+    public void setHasDualSim(boolean hasDualSim) {
+        this.hasDualSim = hasDualSim;
+    }
+
+    public boolean isHasFlashlight() {
+        return hasFlashlight;
+    }
+
+    public void setHasFlashlight(boolean hasFlashlight) {
+        this.hasFlashlight = hasFlashlight;
+    }
+
+    @Override
+    public String toString() {
+        return "KeypadPhone{" +
+                "brand='" + getBrand() + '\'' +
+                ", model='" + getModel() + '\'' +
+                ", price=" + getPrice() +
+                ", hasDualSim=" + hasDualSim +
+                ", hasFlashlight=" + hasFlashlight +
+                '}';
+    }
+}
---
+++ src/main/java/org/example/SmartPhone.java
@@ -0,0 +1,47 @@
+package org.example;
+
+/**
+ * Клас для смартфонів (похідний від Phone).
+ */
+public class SmartPhone extends Phone {
+    private double cameraMegapixels;
+    private boolean hasNFC;
+
+    public SmartPhone(String brand, String model, double price, int year, int storage,
+            int batteryCapacity, OperatingSystem operatingSystem, double weight, Color color,
+            double cameraMegapixels, boolean hasNFC) {
+        super(brand, model, price, year, storage, batteryCapacity, operatingSystem, weight, color);
+        setCameraMegapixels(cameraMegapixels);
+        this.hasNFC = hasNFC;
+    }
+
+    public double getCameraMegapixels() {
+        return cameraMegapixels;
+    }
+
+    public void setCameraMegapixels(double cameraMegapixels) {
+        if (cameraMegapixels < 0) {
+            throw new IllegalArgumentException("Кількість мегапікселів повинна бути більше 0");
+        }
+        this.cameraMegapixels = cameraMegapixels;
+    }
+
+    public boolean isHasNFC() {
+        return hasNFC;
+    }
+
+    public void setHasNFC(boolean hasNFC) {
+        this.hasNFC = hasNFC;
+    }
+
+    @Override
+    public String toString() {
+        return "SmartPhone{" +
+                "brand='" + getBrand() + '\'' +
+                ", model='" + getModel() + '\'' +
+                ", price=" + getPrice() +
+                ", cameraMegapixels=" + cameraMegapixels +
+                ", hasNFC=" + hasNFC +
+                '}';
+    }
+}
